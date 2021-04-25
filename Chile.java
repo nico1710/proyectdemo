cce95a14753f37bd3514d425055b7c5dab57d96a
Nicolas <nicopereiran7n7n7@gmail.com>
2021-04-25 18:25:33 -0400

    segundo commit

--- Chile.java
+++
@@ -1,5 +0,0 @@
-public class Chile extends Pais{
-    public Chile(String nombre) {
-        super(nombre);
-    }
-}
--- Main.java
+++ Main.java
@@ -1,8 +1,40 @@
 import java.util.Scanner;
 
 public class Main {
-
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+        double nota;
+        int opcion;
+        Formula formula = new Formula();
+        Scanner teclado = new Scanner(System.in);
 
+        System.out.println("Selecione pais");
+        System.out.println("1.- Argentina");
+        System.out.println("2.- Peru");
+        System.out.println("3.- Bolivia");
+
+        opcion = teclado.nextInt();
+
+        switch(opcion){
+            case 1:
+                System.out.println("Ingrese nota ");
+                nota = teclado.nextDouble();
+                System.out.println(formula.formulaArgentina(nota));
+                break;
+
+            case 2:
+                System.out.println("Ingrese nota ");
+                nota = teclado.nextDouble();
+                System.out.println(formula.formulaPeruana(nota));
+                break;
+
+            case 3:
+                System.out.println("Ingrese nota ");
+                nota = teclado.nextDouble();
+                System.out.println(formula.formulaBoliviana(nota));
+                break;
+        }
     }
+
+
 }
